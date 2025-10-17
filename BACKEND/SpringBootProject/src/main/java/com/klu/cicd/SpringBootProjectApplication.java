70faf7bf926c81b666ea3272845f702e2ef9ea55
PARDHU <pardhu22092006@gmail.com>
2025-10-17 12:44:10 +0530

    Modified pom.xml

--- BACKEND/SpringBootProject/src/main/java/com/klu/cicd/SpringBootProjectApplication.java
+++ BACKEND/SpringBootProject/src/main/java/com/klu/cicd/SpringBootProjectApplication.java
@@ -2,9 +2,10 @@ package com.klu.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootProjectApplication {
+public class SpringBootProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
