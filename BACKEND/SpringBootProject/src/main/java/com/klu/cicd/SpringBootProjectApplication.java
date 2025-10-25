49c3e25c08f356787b6af6bb4cb49373f7e94378
Sai04032006 <pulimilokesh11@gmail.com>
2025-10-25 15:33:17 +0530

    Modified

--- BACKEND/SpringBootProject/src/main/java/com/klu/cicd/SpringBootProjectApplication.java
+++ BACKEND/SpringBootProject/src/main/java/com/klu/cicd/SpringBootProjectApplication.java
@@ -2,13 +2,19 @@ package com.klu.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootProjectApplication  {
+public class SpringBootProjectApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootProjectApplication.class, args);
-		System.out.println("Bhaag rha haii....");
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringBootProjectApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+        System.out.println("Bhaag rha haii...!!!");
+    }
 }
